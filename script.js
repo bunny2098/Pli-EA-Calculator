4ad42803f47a8ecb892f0a7be1d4a8d69f246bf0
bunny2098 <73228963+bunny2098@users.noreply.github.com>
2026-02-11 21:09:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+﻿function calculateEA() {
+  const premium = document.getElementById("premium").value;
+  const term = document.getElementById("term").value;
+  const result = document.getElementById("result");
+
+  if (premium === "" || term === "") {
+    result.innerHTML = "❌ Please enter all values";
+    return;
+  }
+
+  const yearly = premium * 12;
+  const total = yearly * term;
+
+  result.innerHTML = `✅ Expected Amount: ₹ ${total.toLocaleString()}`;
+}
